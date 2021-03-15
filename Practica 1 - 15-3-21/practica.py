906f6299f63deac3b7451ac4a9d84e1fd6c34785
Juan Cruz Junghanss <juan.ik4@gmail.com>
2021-03-15 13:48:57 -0300

    Update practica

--- Practica 1 - 15-3-21/practica.py
+++ Practica 1 - 15-3-21/practica.py
@@ -1,9 +1,20 @@
+# Some practical reminders before starting the course
+
+# Working with lists:
 mylist = [4,3.0,'abc',5,8,-3,0,2]
 
-#Slicing: obtain lists parts by indexing
+# Slicing: obtain a list part by indexing
 print(mylist[:3])
-
 print(mylist[2:4])
-
 print(mylist[5:])
+
+# 
+mylist.extend(['AB','CD','EF'])
+mylist.insert(2,'qwerty')
+print(mylist)
+
+print(mylist.index('AB'))   # Obtain elemnt index
+
+mylist.sort()
+mylist.reverse()
 
